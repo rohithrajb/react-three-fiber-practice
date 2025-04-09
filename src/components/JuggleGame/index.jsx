2b408baef6aacb8fd86594cfc196993881273f97
Rohith Bingi <rohith.bingi_nuv@external.swiggy.in>
2025-04-10 01:12:00 +0530

    balls juggle on click

--- src/components/JuggleGame/index.jsx
+++ src/components/JuggleGame/index.jsx
@@ -1,65 +1,85 @@
-import { Environment, OrbitControls, useHelper } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Physics, RigidBody } from "@react-three/rapier";
-import { Suspense, useRef } from "react";
+import { useRef } from "react";
 import * as THREE from "three";
 
-function Wall({ rotation, position }) {
+function Wall({ rotation, position, height = 1 }) {
   return (
     <RigidBody type="fixed">
       <mesh scale={5} rotation={rotation} position={position} receiveShadow>
-        <planeGeometry />
+        <planeGeometry args={[1, height]} />
         <meshStandardMaterial color={"grey"} />
       </mesh>
     </RigidBody>
   );
 }
 
-function Scene() {
-  const lightRef = useRef(null);
+function Ball({ position, color = "red", scale = 0.3 }) {
+  const ballRef = useRef();
 
-  useHelper(lightRef, THREE.SpotLightHelper, "red");
+  return (
+    <RigidBody ref={ballRef} colliders="ball" restitution={1}>
+      <mesh
+        scale={scale}
+        position={position}
+        castShadow
+        onPointerDown={(contact) => {
+          ballRef.current.setLinvel({ x: -contact.normal.x, y: 3, z: 0 });
+        }}
+      >
+        <sphereGeometry />
+        <meshStandardMaterial roughness={0.3} color={color} />
+      </mesh>
+    </RigidBody>
+  );
+}
+
+function Scene() {
   return (
     <>
       <OrbitControls />
-      <spotLight ref={lightRef} color="white" castShadow />
-      {/* <Suspense> */}
-        <Physics debug>
-          <RigidBody colliders="ball">
-            <mesh scale={0.3} position={[0, 2, 0]} castShadow>
-              <sphereGeometry />
-              <meshStandardMaterial roughness={0.3} color={"brown"} />
-            </mesh>
-          </RigidBody>
-          {/* floor */}
-          <Wall
-            rotation={[THREE.MathUtils.degToRad(-90), 0, 0]}
-            position={[0, -2.5, 0]}
-          />
-          {/* roof */}
-          <Wall
-            rotation={[THREE.MathUtils.degToRad(90), 0, 0]}
-            position={[0, 2.5, 0]}
-          />
-          {/* wall away from camera */}
-          <Wall position={[0, 0, -2.5]} />
-          {/* wall near camera */}
-          <Wall
-            rotation={[THREE.MathUtils.degToRad(180), 0, 0]}
-            position={[0, 0, 2.5]}
-          />
-          {/* wall on the left */}
-          <Wall
-            rotation={[0, THREE.MathUtils.degToRad(90), 0]}
-            position={[-2.5, 0, 0]}
-          />
-          {/* wall on the right */}
-          <Wall
-            rotation={[0, THREE.MathUtils.degToRad(-90), 0]}
-            position={[2.5, 0, 0]}
-          />
-        </Physics>
-      {/* </Suspense> */}
+      <spotLight
+        intensity={50}
+        position={[1, 10, 0]}
+        color="white"
+        angle={THREE.MathUtils.degToRad(50)}
+        castShadow
+      />
+      <Physics gravity={[0, -2, 0]}>
+        <Ball position={[1, 2.5, 0]} scale={0.6} />
+        <Ball position={[-1, 1, 0]} scale={0.9} />
+        {/* floor */}
+        <Wall
+          rotation={[THREE.MathUtils.degToRad(-90), 0, 0]}
+          position={[0, -5, 0]}
+        />
+        {/* roof */}
+        <Wall
+          rotation={[THREE.MathUtils.degToRad(90), 0, 0]}
+          position={[0, 5, 0]}
+        />
+        {/* wall away from camera */}
+        <Wall position={[0, 0, -2.5]} height={2} />
+        {/* wall near camera */}
+        <Wall
+          rotation={[THREE.MathUtils.degToRad(180), 0, 0]}
+          position={[0, 0, 2.5]}
+          height={2}
+        />
+        {/* wall on the left */}
+        <Wall
+          rotation={[0, THREE.MathUtils.degToRad(90), 0]}
+          position={[-2.5, 0, 0]}
+          height={2}
+        />
+        {/* wall on the right */}
+        <Wall
+          rotation={[0, THREE.MathUtils.degToRad(-90), 0]}
+          position={[2.5, 0, 0]}
+          height={2}
+        />
+      </Physics>
       <Environment preset="apartment" background blur={0.2} />
     </>
   );
@@ -68,7 +88,7 @@ function Scene() {
 export default function JuggleGame() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas camera={{ position: [0, 2, 5] }} shadows>
+      <Canvas camera={{ position: [0, 0, 8] }} shadows>
         <Scene />
       </Canvas>
     </div>
